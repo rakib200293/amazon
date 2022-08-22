69c5f638829da07399a8fc5ad153df9b85782af1
Md. Rakib Islam <rakibislam.nitex@gmail.com>
2022-08-22 14:11:31 +0600

    strict mood removed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Products from "../../fakeData/products.json";
-
+import "./shop.css";
 
 const Shop = () => {
-    useEffect( () => {
-       const allProducts =  Products.slice(0, 10);
-    }, [])
-    
+    const [products, setproducts] = useState([]);
+    useEffect(() => {
+        const allProducts = Products.slice(0, 10);
+        setproducts(allProducts);
+    }, []);
+
     return (
         <div>
-            
+            <div className="flex">
+                <div className="w-90 p-4">
+                    <ul>
+                        {products.map((pd) => (
+                            <li>{pd.name}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="w-10 p-4"></div>
+            </div>
         </div>
     );
 };
